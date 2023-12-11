efea73672ce2fc82fd854690c61d3f6e633f0ef5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-11 21:53:51 +0530

    add button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { myContext } from '../context/context';
+
+const Button = ({ type,name, isActive, handleClick ,fromProjects }) => {
+  const { updateContent, updateType } = useContext(myContext);
+
+  const handleClickEvent = (e) => {
+    updateType(e.target.name);
+    updateContent(e.target.name);
+    handleClick(); // This triggers the handleClick function from the Sidebar
+  };
+if(fromProjects){
+  return  <button
+  className={`h-10 w-[100px] rounded-2xl m-2  border-[1px] text-white  hover:bg-white hover:text-black hover:border-black
+  }`}
+  onClick={handleClickEvent}
+  name={name}
+>
+  {name}
+</button>
+}
+  return (
+    <button
+      className={`h-10 w-[120px] rounded-2xl m-2  border-[1px] text-white  hover:bg-white hover:text-black hover:border-black ${
+        isActive ? 'opacity-100 bg-gradient-to-t from-purple-800 via-violet-900 to-purple-800' : 'opacity-50'
+      }`}
+      onClick={handleClickEvent}
+      name={type.name}
+    >
+      {type.name}
+    </button>
+  );
+};
+
+export default Button;
