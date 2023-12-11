282bb1ae53c14921812995463f25b8285615d952
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-11 21:53:28 +0530

    add about section

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='h-full w-full text-white flex flex-col justify-center items-start p-[50px]'>
+    <h1>SARTHAK SANJAY</h1>
+    <h2>I am a Fullstack Developer</h2>
+    <p>
+        I am a fullstack developer an opensource contributer alkfdkldfafndkn
+        afnkndnf abjfdbj adhkjka  ajofdoa afaj alkfdkldfafndkn
+        afnkndnf abjfdbj adhkjka  ajofdoa afajalkfdkldfafndkn
+        afnkndnf abjfdbj adhkjka  ajofdoa afaj
+    </p>
+    </div>
+  )
+}
+
+export default About
