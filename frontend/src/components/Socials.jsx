20f8c2108141fe8ed1bb950c65b7fde59d97eb63
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-12 22:22:01 +0530

    add social links

---
+++ frontend/src/components/Socials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+const Socials = () => {
+  return (
+    <div className='h-[100px] w-full flex gap-3 '>
+         <SocialIcon url='https://github.com/SarthakSanjay'/>
+        <SocialIcon url='https://x.com/SarthakSanjay01'/>
+        <SocialIcon url='https://www.linkedin.com/in/sarthak-sanjay-922828240/'/>
+    </div>
+  )
+}
+
+export default Socials
