4f4ab99a452381789f367213372e6fefd32f64be
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-12 22:20:15 +0530

    update file

--- frontend/src/components/SkillBox.jsx
+++ frontend/src/components/SkillBox.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { FaNodeJs, FaHtml5, FaCss3, FaGit } from "react-icons/fa";
+import { SiAxios ,SiExpress, SiJavascript, SiMongodb, SiMongoose, SiReact } from "react-icons/si";
+const SkillBox = ({ item }) => {
+  const getIcon = (name) => {
+    switch (name.toLowerCase()) {
+      case 'html':
+        return <FaHtml5 />;
+      case 'css':
+        return <FaCss3 />;
+      case 'javascript':
+        return <SiJavascript />;
+      case 'react':
+        return <SiReact />;
+      case 'axios':
+        return <SiAxios />;
+      case 'node.js':
+        return <FaNodeJs />;
+      case 'mongodb':
+        return <SiMongodb />;
+      case 'mongoose':
+        return <SiMongoose />;
+      case 'express':
+        return <SiExpress />;
+      case 'git':
+        return <FaGit />;
+      default:
+        return null;
+    }
+  };
 
-const SkillBox = ({item}) => {
   return (
     <div
-     className='h-[40px] w-[120px]  text-white border-[1px] bg-opacity-10 flex justify-center items-center rounded-lg opacity-50 hover:opacity-100'
-     style={{
-        borderColor: `${item.color}`,
-        boxShadow: `0px 0px 10px 5px ${item.color}`,
-     }}
-     >{item.name}</div>
-  )
-}
+      className='h-[100px] w-[120px] text-white border-[1px] bg-opacity-10 flex flex-col justify-center items-center rounded-lg opacity-50 hover:opacity-100'
+    
+    >
+    <div className='text-3xl flex justify-center items-center flex-col' style={{
+        
+        color: `${item.color}`,
+        filter: `
+         drop-shadow(0 0 10px ${item.color})
+          drop-shadow(0 0 20px ${item.color})
+           `
+}}>
 
-export default SkillBox
+      {getIcon(item.name)}
+    </div>
+      <p className='text-[12px] text-white'>{item.name}</p>
+      
+    </div>
+  );
+};
+
+export default SkillBox;
