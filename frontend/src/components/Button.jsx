ee62d733ef141b46d749fdda9049830c76052ae0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-13 20:19:53 +0530

    add responsive classes

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ import { myContext } from '../context/context';
 
 const Button = ({ type,name, isActive, handleClick ,fromProjects }) => {
   const { updateContent, updateType } = useContext(myContext);
-
+  
   const handleClickEvent = (e) => {
     updateType(e.target.name);
     updateContent(e.target.name);
@@ -21,7 +21,7 @@ if(fromProjects){
 }
   return (
     <button
-      className={`h-10 w-[120px] rounded-2xl m-2  border-[1px] text-white  hover:bg-white hover:text-black hover:border-black ${
+      className={`h-10 w-5/6 lg:w-[120px] rounded-2xl m-2  border-[1px] text-white  hover:bg-white hover:text-black hover:border-black ${
         isActive ? 'opacity-100 bg-gradient-to-t from-purple-800 via-violet-900 to-purple-800' : 'opacity-50'
       }`}
       onClick={handleClickEvent}
