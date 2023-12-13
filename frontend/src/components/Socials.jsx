068dcd367404abd4c3af86b352dc97503df7e6f3
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-13 21:00:18 +0530

    add gmail

--- frontend/src/components/Socials.jsx
+++ frontend/src/components/Socials.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 const Socials = () => {
   return (
-    <div className='h-[100px] w-full flex gap-3 '>
-         <SocialIcon url='https://github.com/SarthakSanjay'/>
-        <SocialIcon url='https://x.com/SarthakSanjay01'/>
-        <SocialIcon url='https://www.linkedin.com/in/sarthak-sanjay-922828240/'/>
+    <div className='h-[100px] lg:h-1/6 w-full flex lg:flex-col gap-3 '>
+         <SocialIcon target='_blank' url='https://github.com/SarthakSanjay'/>
+        <SocialIcon target='_blank' url='https://x.com/SarthakSanjay01'/>
+        <SocialIcon target='_blank' url='https://www.linkedin.com/in/sarthak-sanjay-922828240/'/>
+        <SocialIcon target='_blank' bgColor='red' url='mailto:sarthaksanjaycoll@gmail.com' />
     </div>
   )
 }
