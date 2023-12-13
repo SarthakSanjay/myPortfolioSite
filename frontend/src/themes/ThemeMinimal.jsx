a362d3c94c1d608e221ebed974f2c6341e1fda56
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-13 21:02:30 +0530

    add responsive classes

--- frontend/src/themes/ThemeMinimal.jsx
+++ frontend/src/themes/ThemeMinimal.jsx
@@ -4,8 +4,8 @@ import Sidebar from '../components/Sidebar'
 import Contents from '../components/Contents'
 const ThemeMinimal = () => {
   return (
-    <div className='app2 h-screen w-screen bg-white  flex'>
-       <Sidebar />
+    <div className='app2 h-fit lg:h-screen w-screen  bg-white  flex flex-col lg:flex-row' >
+      <Sidebar />
        <Contents />
     </div>
   )
